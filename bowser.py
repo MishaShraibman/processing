eba1cef927d6e0d459a8bae129f0585c17b4781e
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-09-06 20:31:17 +0700

    who i am?

---
+++ bowser.py
@@ -0,0 +1,10 @@
+import pyautogui
+print(pyautogui.position())
+pyautogui.moveTo(831,1059)
+pyautogui.click()
+pyautogui.moveTo(1424,17)
+pyautogui.click()
+pyautogui.moveTo(707,62)
+pyautogui.click()
+pyautogui.write('hfpevty kb z?')
+pyautogui.press('enter')
