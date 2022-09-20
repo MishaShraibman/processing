648b7d568c6d7a8e7bd266ae37d236bdc4391b84
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-09-20 19:11:11 +0700

    the one of the most collest shit what i've ever done

---
+++ goven kokoito.py
@@ -0,0 +1,4 @@
+a = input("ok, now give me your password: ")
+while a != "gachimuchi":
+    a = input("i think you're got the wrong door... Leatherclub two blocks down, password: ")
+print("Come on, let's go")
