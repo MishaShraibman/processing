89febb446456d3a71a44582f13d0ff6d28b17c2c
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-09-20 20:06:03 +0700

    elif иначе

---
+++ Bruh.py
@@ -0,0 +1,10 @@
+a = input("osu user name: ")
+b = input("password: ")
+while a != "WhiteCat" or b != "123567NhP":
+    if a != "WhiteCat":
+        print("wrong login, retry")      
+    elif b != "123567NhP":
+        print("wrong password, retry")
+    a = input("osu user name: ")
+    b = input("password: ")
+print("gay detected")
