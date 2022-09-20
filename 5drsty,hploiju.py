3fcbb6f9e586f8c470144c2d8a1b26a1bcf6b05f
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-09-20 19:44:55 +0700

    osu players, it's for you (and for me)

---
+++ 5drsty,hploiju.py
@@ -0,0 +1,7 @@
+a = input("osu user name: ")
+b = input("password: ")
+while a != "WhiteCat" or b != "123567NhP":
+    print('retry: ')
+    a = input("osu user name: ")
+    b = input("password: ")
+print("gay detected")
