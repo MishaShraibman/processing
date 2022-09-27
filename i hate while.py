a78b3875025e73a2766790ffd5cbf04928a7a1fb
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-09-27 18:48:23 +0700

    Update i hate while.py

--- i hate while.py
+++ i hate while.py
@@ -3,5 +3,5 @@ import random
 a = int(input("угадай число 1-100: "))
 b = random.randint(1, 9)
 while a != b:
-    a = input(" ")
+    a = int(input(" "))
 print("верно, а теперь иди в свою писочницу мелкишь")
