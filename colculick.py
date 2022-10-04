39feeaab95c395ccabd3b60ef3ebd4eb63ab96e2
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-10-04 19:49:46 +0700

    gottem

--- colculick.py
+++ colculick.py
@@ -1,20 +1,22 @@
 comand = input('введите команду: ')
 while comand != 'exit':
     if comand == '+':
-        num1 = int(input('1 слагаемое: '))
-        num2 = int(input('2 слагаемое: '))
-        print(num1 + num2)
+        num1 = float(input('1 слагаемое: '))
+        num2 = float(input('2 слагаемое: '))
+        prfloat(num1 + num2)
     elif comand == '-':
-        num1 = int(input('уменьшаемое: '))
-        num2 = int(input('вычитаемое: '))
-        print(num1 - num2)
+        num1 = float(input('уменьшаемое: '))
+        num2 = float(input('вычитаемое: '))
+        prfloat(num1 - num2)
     elif comand == '*':
-        num1 = int(input('1 множитель: '))
-        num2 = int(input('2 множитель: '))
-        print(num1 * num2)
+        num1 = float(input('1 множитель: '))
+        num2 = float(input('2 множитель: '))
+        prfloat(num1 * num2)
     elif comand == '/':
-        num1 = int(input('делимое: '))
-        num2 = int(input('делитель: '))
-        print(num1 / num2)
+        num1 = float(input('делимое: '))
+        num2 = float(input('делитель: '))
+        prfloat(num1 / num2)
     else:
-        comand = input('введите команду: ')
+        prfloat('команда не обноружена')
+    comand = input('введите команду: ')
+prfloat("выклюяем")
