550144096870426e4149afd3b6b6dd2bc4f31113
EDGE.txt <101643689+EDGEtxt@users.noreply.github.com>
2022-10-04 19:53:28 +0700

    this is sneeky peeky like

--- colculick.py
+++ colculick.py
@@ -3,20 +3,20 @@ while comand != 'exit':
     if comand == '+':
         num1 = float(input('1 слагаемое: '))
         num2 = float(input('2 слагаемое: '))
-        prfloat(num1 + num2)
+        print(num1 + num2)
     elif comand == '-':
         num1 = float(input('уменьшаемое: '))
         num2 = float(input('вычитаемое: '))
-        prfloat(num1 - num2)
+        print(num1 - num2)
     elif comand == '*':
         num1 = float(input('1 множитель: '))
         num2 = float(input('2 множитель: '))
-        prfloat(num1 * num2)
+        print(num1 * num2)
     elif comand == '/':
         num1 = float(input('делимое: '))
         num2 = float(input('делитель: '))
-        prfloat(num1 / num2)
+        print(num1 / num2)
     else:
-        prfloat('команда не обноружена')
+        print('команда не обноружена')
     comand = input('введите команду: ')
-prfloat("выклюяем")
+print("выклюяем")
